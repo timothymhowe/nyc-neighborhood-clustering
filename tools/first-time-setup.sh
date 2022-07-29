d16aad7237bf024151033901f68a89da3aaaa7ff
Timothy M. Howe <thowe1923@gmail.com>
2022-07-29 19:00:28 -0400

    Created bash scripts to make project setup and exporting of environments easier.  `first-time-setup.sh` creates a new conda environment for the project.  `tools/export-NYC-Env.sh` exports the project environment to file.

--- tools/first-time-setup.sh
+++ tools/first-time-setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd `git rev-parse --show-toplevel`
-echo "configuring git hooks..."
-cp .nyc_gitHooks/pre-commit .git/hooks
-
+echo "configuring git hooks"
+# cp .nyc_gitHooks/pre-commit .git/hooks
+chmod a+x .nyc_gitHooks/*
+# cp .nyc_gitHooks/*  .git/hooks
+git config --local core.hooksPath .nyc_gitHooks
+echo "...done!"
 echo "Creating conda environment 'NYC-Env' from '.config/environment.yml'..."
 conda env create --name NYC-Env --file=.config/environment.yml
 
