ed40e1ddc3f77ac88a28a6422f8563b03cd5ff2c
Timothy M. Howe <thowe1923@gmail.com>
2022-07-30 11:38:55 -0400

    Removed accidental `.config/environments.yml` file.  Changed `/tools/export-NYC-Env.sh` to remove version numbers.

--- tools/export-NYC-Env.sh
+++ tools/export-NYC-Env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `git rev-parse --show-toplevel` 
-conda env export -n NYC-Env --no-build | grep -v "^prefix: " > ../.config/environment.yml
+conda env export -n NYC-Env --from-history --no-build | grep -v "^prefix: " > .config/environment.yml
 
 echo "Exported environment to '.config/environment.yml'"
